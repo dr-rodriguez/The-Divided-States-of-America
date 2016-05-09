ff350752c43ac3c8c7901ae012b5a8847911b957
David Rodriguez <dr-rodriguez@users.noreply.github.com>
2016-05-08 21:38:06 -0400

    first pass at grabbing user tweets

--- create_model.py
+++ create_model.py
@@ -1,7 +1,9 @@
 # Script to read in tweets and process them
 
 import requests
+import simplejson
 from requests_oauthlib import OAuth1 # used for twitter's api
+from pandas.io.json import json_normalize
 
 with open("twitter_secrets.json.nogit") as f:
     secrets = simplejson.loads(f.read())
@@ -15,7 +17,27 @@ auth = OAuth1(
 )
 
 # TODO: Grab most recent ~2000 user tweets for Hillary and Trump
+# Load user data
+page = 0
 url = 'https://api.twitter.com/1.1/statuses/user_timeline.json'
+screen_name = 'strakul'
+for i in range(2):
+    if page == 0:
+        params = {'screen_name': screen_name, 'count': 10, 'exclude_replies': 'true', 'include_rts': 'false'}
+    else:
+        max_id = data[-1]['id'] - 1
+        params = {'screen_name': screen_name, 'count': 10, 'exclude_replies': 'true', 'include_rts': 'false',
+                  'max_id': max_id}
+
+    r = requests.get(url, auth=auth, params=params)
+    data = simplejson.loads(r.text)
+
+    if page == 0:
+        df = json_normalize(data)
+    else:
+        df = df.append(json_normalize(data), ignore_index=True)
+
+    page += 1
 
 # TODO: Merge tweets together, get 100-200 most common terms
 
