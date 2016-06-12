71c8142a4392a374f9c2c0234d69a35ccce1a5cc
David Rodriguez <dr-rodriguez@users.noreply.github.com>
2016-06-12 17:31:40 -0400

    eliminated CS19 tracking

--- tweet_builder.py
+++ tweet_builder.py
@@ -28,13 +28,6 @@ query = 'politic OR trump OR hillary OR clinton OR election'
 s.search(query, max_tweets)
 s.save()
 
-# For fun, gather CS19 tweets
-s2 = TweetLoader(filename='coolstars.json', track_location=False, path='coolstars19/data/')
-s2.load()
-query = '#CS19'
-s2.search(query, 2000, hard_remove=False, remove_rts=False)
-s2.save()
-
 print(time.strftime("%Y-%m-%d"))
 print('{} tweets gathered for Hillary Clinton'.format(len(h.tweets)))
 print('{} tweets gathered for Donald Trump'.format(len(t.tweets)))
